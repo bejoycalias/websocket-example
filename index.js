16d80adf18363b88fc7eb6f2e673996173eacfb3
Bejoy Alias <bejoy.alias@in.ibm.com>
2020-05-16 16:50:29 +0530

    WS SSL

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const WebSocket = require('ws')
+'use strict';
 
-const wss = new WebSocket.Server({
-  port: process.env.WS_PORT || 8080,
+const https = require('https');
+const fs = require('fs');
+const WebSocket = require('ws');
+
+const server = https.createServer({
+  cert: fs.readFileSync('./selfsigned.crt'),
+  key: fs.readFileSync('./selfsigned.key')
+  port: process.env.WS_PORT || 8443,
   path: '/ws'
-})
+});
+
+const wss = new WebSocket.Server({ server });
+
 console.log(`WS Started`)
+
+
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
   console.log('Connection received.')
@@ -14,6 +26,16 @@ wss.on('connection', (ws) => {
     console.log(`Received message => ${message}`)
   })
 })
+
+server.listen(function listening() {
+  const ws = new WebSocket(`wss://localhost:${server.address().port}`, {
+    rejectUnauthorized: false
+  });
+
+  ws.on('open', function open() {
+    ws.send('All glory to WebSockets!');
+  });
+});
 
 wss.on('error', (err) => {
   console.log('Error ' + err)
